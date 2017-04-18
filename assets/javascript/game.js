ee68d03bb482aea3f70fe21b210395066b514af1
Michael Bottalico <michaelbottalico@Michaels-MBP.home>
2017-04-18 14:56:18 -0400

    updated

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+var computerChoices = [Math.floor(Math.random() * 120)];
+
+var wins = 0;
+var losses = 0;
+var yourNumbers = [];
+var possibleNumbers = null;
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+$('.images').click(function(event) {
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
