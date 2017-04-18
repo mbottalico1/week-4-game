79f6083dbd236869b1ffa1c38acd1116a49d00aa
Michael Bottalico <michaelbottalico@Michaels-MacBook-Pro.local>
2017-04-18 18:24:19 -0400

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,37 @@
 $(document).ready(function(){
 
-var computerChoices = Math.floor(Math.random() * 120);
+var computerChoices = Math.floor(Math.random() * 109 + 11);
 
 var wins = 0;
 var losses = 0;
 var total = 0;
 
-var crystals = Math.floor(Math.random * 12);
+var crystalOne = Math.floor(Math.random * 12 + 1);
+var crystalTwo = Math.floor(Math.random * 12 + 1);
+var crystalThree = Math.floor(Math.random * 12 + 1);
+var crystalFour = Math.floor(Math.random * 12 + 1);
 
-$('.images').click(function(event) {
-	total = total + crystals;
+
+$('#crystal1').click(function(event) {
+	total = total + crystalOne;
+	
 	
 }
 
+$('#crystal2').click(function(event) {
+	total = total + crystalTwo;
 
+}
 
+$('#crystal3').click(function(event) {
+	total = total + crystalThree;
 
+}
 
+$('#crystal4').click(function(event) {
+	total = total + crystalFour;
 
-
-
+}
 
 
 
