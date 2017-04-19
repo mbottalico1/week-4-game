68676c51f04ed21e0715855acdfd7df55418f498
Michael Bottalico <michaelbottalico@Michaels-MBP.home>
2017-04-19 12:27:07 -0400

    updated some javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,84 @@
-$(document).ready(function(){
-
-var computerChoices = Math.floor(Math.random() * 109 + 11);
-
+var computerChoices = Math.floor((Math.random() * 101) + 19);
 var wins = 0;
 var losses = 0;
-var total = 0;
+var myTotal = 0;
 
 var crystalOne = Math.floor(Math.random * 12 + 1);
 var crystalTwo = Math.floor(Math.random * 12 + 1);
 var crystalThree = Math.floor(Math.random * 12 + 1);
 var crystalFour = Math.floor(Math.random * 12 + 1);
 
+var updateTotal = function() {
+
+	$('#empty-div2').empty();
+	$('#empty-div2').append(myTotal);
+
+	$('#winning').empty();
+	$('#winning').append(wins);
+
+	$('#losing').empty();
+	$('#losing').append(losses);
+}
+
+
+var game = function() {
+	if (myTotal === computerChoices) {
+		wins++;
+		alert('YOU WIN!');
+		restart();
+	} else if(myTotal > computerChoices) {
+		losses++;
+		alert('YOU LOSE! Maybe next time!');
+		restart();
+	} else {
+		updateTotal();
+    }
+}
+
+$('#empty-div1').append(computerChoices);
+$('#empty-div2').append(myTotal);
+
+
+$(document).ready(function(){
 
 $('#crystal1').click(function(event) {
-	total = total + crystalOne;
+	myTotal = myTotal + crystalOne;
+	game();
 	
-	
-}
+})
 
 $('#crystal2').click(function(event) {
-	total = total + crystalTwo;
-
-}
+	myTotal = myTotal + crystalTwo;
+	game();
+})
 
 $('#crystal3').click(function(event) {
-	total = total + crystalThree;
-
-}
+	myTotal = myTotal + crystalThree;
+	game();
+})
 
 $('#crystal4').click(function(event) {
-	total = total + crystalFour;
+	myTotal = myTotal + crystalFour;
+	game();
+})
+
+});
+
+
+var restart = function() {
+	var crystalOne = Math.floor(Math.random * 12 + 1);
+	var crystalTwo = Math.floor(Math.random * 12 + 1);
+	var crystalThree = Math.floor(Math.random * 12 + 1);
+	var crystalFour = Math.floor(Math.random * 12 + 1);
+	updateTotal();
+
+    myTotal = 0;
+	var computerChoices = Math.floor(Math.random() * 101 + 19);
+
+	$('#empty-div1').empty();
+	$('#empty-div1').append(computerChoices);
 
 }
-
-
-
-
-
-
 
 
 
